8dcd77a8ea8561980a1b2e95d813dc5dc08224c8
Leon <leon.lafa@gmail.com>
2020-10-04 13:36:47 +0100

    add react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import TasksList from './components/TaskList';
@@ -30,21 +31,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My Tasks</h1>
+        <Router>
+          <Switch>
+            <Route exact path="/" >
+              <h1>My Tasks</h1>
 
-        <TasksList ref="tasksList" />
+              <TasksList ref="tasksList" />
 
-        <form onSubmit={this.clickAddTask}>
-          <input
-            type="text"
-            size="30"
-            placeholder="New Task"
-            name="newTaskTitle"
-            value={this.state.newTaskTitle}
-            onChange={this.handleChange}
-          />
-          <input className="btn-primary" type="submit" value="Add" />
-        </form>
+              <form onSubmit={this.clickAddTask}>
+                <input
+                  type="text"
+                  size="30"
+                  placeholder="New Task"
+                  name="newTaskTitle"
+                  value={this.state.newTaskTitle}
+                  onChange={this.handleChange}
+                />
+                <input className="btn-primary" type="submit" value="Add" />
+              </form>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
