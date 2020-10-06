eda0a9b29e835f1a52074e304337f189583ac2c1
James Vickery <dev@jamesvickery.net>
2020-10-06 01:03:30 +0100

    finish converting to react hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
 
 import TasksList from './components/TaskList';
 
-class App extends Component {
-  state = {
-    newTaskTitle: ''
-  };
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTaskTitle, setNewTaskTitle] = useState('');
 
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+  const getTasks = useCallback(() => {
+    fetch('/api/tasks')
+      .then(res => res.json())
+      .then(setTasks);
+  });
 
-  clickAddTask = event => {
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  const clickAddTask = event => {
     event.preventDefault();
-
-    const title = this.state.newTaskTitle;
 
     fetch('/api/tasks/add', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ title })
+      body: JSON.stringify({ title: newTaskTitle }),
     }).then(() => {
-      this.setState({ newTaskTitle: '' });
-      this.refs.tasksList.getTasks();
+      setNewTaskTitle('');
+      getTasks();
     });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>My Tasks</h1>
+  return (
+    <div className="App">
+      <h1>My Tasks</h1>
 
-        <TasksList ref="tasksList" />
+      <TasksList tasks={tasks} updateTasks={getTasks} />
 
-        <form onSubmit={this.clickAddTask}>
-          <input
-            type="text"
-            size="30"
-            placeholder="New Task"
-            name="newTaskTitle"
-            value={this.state.newTaskTitle}
-            onChange={this.handleChange}
-          />
-          <input className="btn-primary" type="submit" value="Add" />
-        </form>
-      </div>
-    );
-  }
-}
+      <form onSubmit={clickAddTask}>
+        <input
+          type="text"
+          size="30"
+          placeholder="New Task"
+          value={newTaskTitle}
+          onChange={event => setNewTaskTitle(event.target.value)}
+        />
+        <input className="btn-primary" type="submit" value="Add" />
+      </form>
+    </div>
+  );
+};
 
 export default App;
--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,149 +1,49 @@
-import React, { useState, useEffect, useRef, useImperativeHandle , forwardRef} from 'react';
+import React from 'react';
 
-const TaskList = forwardRef((props, ref)=>{
-
-  const [tasks, setTasks] = useState([]);
-  const inputRef = useRef(null);
-  useImperativeHandle(
-    ref,
-    ()=>({
-      getTasks:()=>{
-        getTasks();
-      }
-    })
-  );   
-
-  const getTasks = () => {
-    fetch('api/tasks')
-      .then(res => res.json())
-      .then((tasks) => {
-        setTasks(tasks);
-      });
-  };
-
+const TaskList = ({ tasks, updateTasks }) => {
   const clickDeleteTask = (event, task) => {
     event.preventDefault();
 
     fetch(`/api/tasks/delete/${task._id}`, {
-      method: 'delete'
+      method: 'delete',
     })
       .then(res => res.json())
-      .then(t => {
-        getTasks();
-      });
+      .then(() => updateTasks());
   };
 
-  const toggleDone = (task) => {
-    task.done = !task.done;
-
-    setTasks(tasks);
-
+  const toggleDone = task => {
     fetch(`/api/tasks/update/${task._id}`, {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ done: task.done })
-    });
+      body: JSON.stringify({ done: !task.done }),
+    }).then(() => updateTasks());
   };
 
-    return (
-      <ul className="tasks">
-        {tasks.map(task => (
-          <li key={task._id}>
-            <label className={task.done ? 'done' : ''}>
-              <input ref={inputRef} {...props}
-                type="checkbox"
-                checked={task.done}
-                onChange={toggleDone}
-              />{' '}
-              {task.title}
-              <svg
-                onClick={clickDeleteTask}
-                className="delete-button"
-                width="16"
-                height="16"
-                viewBox="0 0 12 16"
-              >
-                <path d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48L7.48 8z"></path>
-              </svg>
-            </label>
-          </li>
-        ))}
-      </ul>
-   );
-})
+  return (
+    <ul className="tasks">
+      {tasks.map(task => (
+        <li key={task._id}>
+          <label className={task.done ? 'done' : ''}>
+            <input
+              type="checkbox"
+              checked={task.done}
+              onChange={() => toggleDone(task)}
+            />{' '}
+            {task.title}
+            <svg
+              onClick={event => clickDeleteTask(event, task)}
+              className="delete-button"
+              width="16"
+              height="16"
+              viewBox="0 0 12 16"
+            >
+              <path d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48L7.48 8z"></path>
+            </svg>
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 export default TaskList;
-
-
-/*
-export default class TaskList extends Component {
-  state = {
-    tasks: []
-  };
-
-  componentDidMount() {
-    this.getTasks();
-  }
-
-  getTasks = () => {
-    fetch('/api/tasks')
-      .then(res => res.json())
-      .then(tasks => {
-        this.setState({ tasks });
-      });
-  };
-
-  clickDeleteTask = (event, task) => {
-    event.preventDefault();
-
-    fetch(`/api/tasks/delete/${task._id}`, {
-      method: 'delete'
-    })
-      .then(res => res.json())
-      .then(t => {
-        this.getTasks();
-      });
-  };
-
-  toggleDone = task => {
-    task.done = !task.done;
-
-    this.setState({ tasks: this.state.tasks });
-
-    fetch(`/api/tasks/update/${task._id}`, {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ done: task.done })
-    });
-  };
-
-  render() {
-    const { tasks } = this.state;
-
-    return (
-      <ul className="tasks">
-        {tasks.map(task => (
-          <li key={task._id}>
-            <label className={task.done ? 'done' : ''}>
-              <input
-                type="checkbox"
-                checked={task.done}
-                onChange={() => this.toggleDone(task)}
-              />{' '}
-              {task.title}
-              <svg
-                onClick={event => this.clickDeleteTask(event, task)}
-                className="delete-button"
-                width="16"
-                height="16"
-                viewBox="0 0 12 16"
-              >
-                <path d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48L7.48 8z"></path>
-              </svg>
-            </label>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
-*/
