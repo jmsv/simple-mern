6726f66c6b729b1349e0ec5b6dfd3499e8dd57ea
ayush-git228 <66081505+ayush-git228@users.noreply.github.com>
2020-10-07 18:29:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require("./src/config");
+const passport =require("passport");
+
+const url = config.mongoUrl;
+const connect = mongoose.connect(url);   
+
+connect.then((db) =>{
+    console.log("Connected to MongoDB database");
+})
 
 const app = express();
 
+app.use(passport.initialize());  //New
+
 const routeTasks = require('./src/routes/tasks');
+const routeUsers = require('./src/routes/users');
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.json());
 
 app.use('/api/tasks', routeTasks, (req, res) => res.sendStatus(401));
+app.use('/api/users', routeUsers);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
